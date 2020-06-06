d3746f140a4d524b5c3cd0564c20360d63fab95f
danilo_miranda <danilo.miranda@conpec.com.br>
2020-06-06 14:03:04 -0300

    minor fix

--- projeto-fernando-webapp/src/components/Research/Research.jsx
+++ projeto-fernando-webapp/src/components/Research/Research.jsx
@@ -133,7 +133,7 @@ function Research(props) {
         ?(
           <>
             <label>Resultado:</label>
-            {Array.isArray(response)? `${researchData.researchService1}:${response[0]}  ${researchData.researchService2}:${response[1]}` :Math.round(response)}
+            {Array.isArray(response)? `${researchData.researchService1}:${Math.round(response[0])}  ${researchData.researchService2}:${Math.round(response[1])}` :Math.round(response)}
           </>
         )
         :null
