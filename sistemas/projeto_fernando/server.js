4907567bc611806160c123a694971e32a223e7de
GustavoGV <gustavo.g.a@icloud.com>
2021-02-08 16:39:04 -0300

    econder

--- sistemas/projeto_fernando/server.js
+++ sistemas/projeto_fernando/server.js
@@ -16,8 +16,8 @@ import { SSL_OP_EPHEMERAL_RSA } from 'constants'
 //24000000 no total para cada cooperativa a mais no jogo...
 
 
-//mongoose.connect('mongodb://localhost/aluno_teste') //conexão com o banco de dados
-mongoose.connect('mongodb+srv://user_4:batata@toka.rv0te.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority')
+mongoose.connect('mongodb://localhost/aluno_teste') //conexão com o banco de dados
+
 mongoose.connection
     .once('open', () => console.log('Conexao com MongoDB (banco de dados) foi estabelecida com sucesso'))
     .on('error', (error) => {
